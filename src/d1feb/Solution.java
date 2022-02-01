afda04bb9dbdf247b3b950b5df1433080d7cbe7c
Aswhini maurya <ashwinimaurya@paisabazaar.com>
2022-02-02 02:18:22 +0530

    daily code challenges

--- src/d1feb/Solution.java
+++ src/d1feb/Solution.java
@@ -1,35 +1,45 @@
 package d1feb;
 
-import java.util.Arrays;
 
 public class Solution {
-	
-	
+
 	public static void main(String[] args) {
-		
+		System.out.println(Solution.longestPalindrome("babad"));
 	}
-	
-	 public int earliestAcq(int[][] logs, int n) {
-		 
-		 Arrays.sort(logs,(a,b)->Integer.compare(a[0], b[0]));
-		 
-		 int count =n;
-		 int groupCount = n;
-		return groupCount;
-	        
-	        
-	    }
-	
-	
-	 public static int maxProfit(int[] prices) {
-		 
-		 int min = Integer.MAX_VALUE,max= Integer.MIN_VALUE;
-		 for(int price:prices) {
-			 min = Math.min(min, price);
-			 max = Math.max(max, price-min);
-		 }
-		return  max;
-		 
-	 }
+
+	public static String longestPalindrome(String s) {
+		int start = 0, end = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int len1 = expendFromCenter(s, i, i + 1);
+			int len2 = expendFromCenter(s, i, i);
+			int len = Math.max(len1, len2);
+			if (end - start < len) {
+				start = i - (len - 1) / 2;
+				end = i + len / 2;
+			}
+		}
+		return s.substring(start, end+1);
+	}
+
+	static int expendFromCenter(String s, int i, int j) {
+		while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+			i--;
+			j++;
+
+		}
+		return j - i - 1;
+
+	}
+
+	public static int maxProfit(int[] prices) {
+
+		int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+		for (int price : prices) {
+			min = Math.min(min, price);
+			max = Math.max(max, price - min);
+		}
+		return max;
+
+	}
 
 }
